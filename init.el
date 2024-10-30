6353349188642744fc25e6d4a845eecb77820774
James Yang <james.yang92@gmail.com>
2024-10-30 17:00:51 +0000

    Update init.el

--- init.el
+++ init.el
@@ -1,13 +1,13 @@
 (setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
+(setq-default tab-width 4)
 (global-display-line-numbers-mode 1)
 (setq column-number-mode t)
-(setq c-basic-offset 2
+(setq c-basic-offset 4
       indent-tabs-mode nil
-      tab-width 2)
+      tab-width 4)
 (c-set-offset 'case-label '+)
 (add-hook 'python-mode-hook
       (lambda ()
         (setq indent-tabs-mode t)
-        (setq tab-width 2)
-        (setq python-indent-offset 2)))
+        (setq tab-width 4)
+        (setq python-indent-offset 4)))
